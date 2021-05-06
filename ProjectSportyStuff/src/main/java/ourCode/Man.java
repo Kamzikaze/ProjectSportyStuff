3c4aad3b412ec79afb4229502b7f14dbc9d6959e
Teo <teo@example.com>
2021-05-06 10:03:47 +0200

    d

--- ProjectSportyStuff/src/main/java/ourCode/Man.java
+++ ProjectSportyStuff/src/main/java/ourCode/Man.java
@@ -2,9 +2,10 @@ package ourCode;
 
 public class Man extends Athlete{
 
-	public Man(String firstName, String lastName) {
+	public Man(String firstName, String lastName, int id) {
 		this.firstName = firstName;
 		this.lastName = lastName;
+		this.ID = id;
 	}
 
 }
