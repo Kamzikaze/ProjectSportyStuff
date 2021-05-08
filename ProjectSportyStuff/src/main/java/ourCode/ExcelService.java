096faea3f09cd87cbe7c198a1ef0222b6baf848c
Maja Norrman <norrman.ma@gmail.com>
2021-05-08 17:53:12 +0200

    Created new class for excel management. Added boilerplate methods for excel class.

---
+++ ProjectSportyStuff/src/main/java/ourCode/ExcelService.java
@@ -0,0 +1,28 @@
+package ourCode;
+
+public class ExcelService {
+
+	public void AddWomanResults(int athleteID, int eventID, double result) {
+		// TODO:
+	}
+
+	public void AddManResults(int athleteID, int eventID, double result) {
+		// TODO:
+	}
+
+	public void GetIndividualWomanResults(int athleteID) {
+		// TODO:
+	}
+
+	public void GetIndividualManResults(int athleteID) {
+		// TODO:
+	}
+
+	public void AddWomanAthlete(Woman woman) {
+		// TODO:
+	}
+
+	public void AddManAthlete(Man man) {
+		// TODO:
+	}
+}
