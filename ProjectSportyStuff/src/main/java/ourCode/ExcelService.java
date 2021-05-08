afb3495821e32ddfc4b65f15182e20a4d2620567
Maja Norrman <norrman.ma@gmail.com>
2021-05-08 20:29:12 +0200

    Added missing dependencies.

--- ProjectSportyStuff/src/main/java/ourCode/ExcelService.java
+++ ProjectSportyStuff/src/main/java/ourCode/ExcelService.java
@@ -1,28 +1,71 @@
 package ourCode;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.poi.EncryptedDocumentException;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+
 public class ExcelService {
+	private File file;
+	private Workbook workbook;
+	
+	public ExcelService(String filePath) {
+		this.file = new File(filePath);
+		
+		this.checkExistence();
+		
+		this.checkAccess();
+		
+		try {
+			this.workbook = WorkbookFactory.create(this.file);
+		} catch (EncryptedDocumentException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
-	public void AddWomanResults(int athleteID, int eventID, double result) {
+	public void addWomanResults(int athleteID, int eventID, double result) {
 		// TODO:
 	}
 
-	public void AddManResults(int athleteID, int eventID, double result) {
+	public void addManResults(int athleteID, int eventID, double result) {
 		// TODO:
 	}
 
-	public void GetIndividualWomanResults(int athleteID) {
+	public void getIndividualWomanResults(int athleteID) {
 		// TODO:
 	}
 
-	public void GetIndividualManResults(int athleteID) {
+	public void getIndividualManResults(int athleteID) {
 		// TODO:
 	}
 
-	public void AddWomanAthlete(Woman woman) {
+	public void addWomanAthlete(Woman woman) {
 		// TODO:
 	}
 
-	public void AddManAthlete(Man man) {
+	public void addManAthlete(Man man) {
 		// TODO:
+	}
+	
+	private void checkExistence() {
+		if (!this.file.exists()) {
+			// TODO: Create file with correct sheets
+		}
+	}
+	
+	private void checkAccess() {
+		if (!this.file.canRead()) {
+			// throw new Exception("File is not readable");
+		}
+		
+		if (!this.file.canWrite()) {
+			// throw new Exception("File is not writable");
+		}
 	}
 }
