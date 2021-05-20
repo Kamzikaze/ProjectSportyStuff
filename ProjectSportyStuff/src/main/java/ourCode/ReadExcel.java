c40088e82c01fcb25662648266b8ccc1cb12ada7
Teo <teo@example.com>
2021-05-20 10:04:04 +0200

    updated readexcel

--- ProjectSportyStuff/src/main/java/ourCode/ReadExcel.java
+++ ProjectSportyStuff/src/main/java/ourCode/ReadExcel.java
@@ -10,51 +10,52 @@ import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
-public class ReadExcel {
-	
-    	public ArrayList<String> extractAthletes(int columnIndex) {
-            ArrayList<String> athletes = null;
+public class ReadExcel extends Athlete {
+
+    public ArrayList<String> extractAthletes() {
+        ArrayList<String> athletes = null;
+        ArrayList<Athlete> athletes1 = null;
         try {
-        	  File deHe = new File("Excel path here");
-              FileInputStream ex = new FileInputStream(deHe);
-              XSSFWorkbook workbook = new XSSFWorkbook(ex);
-              XSSFSheet sheet = workbook.getSheetAt(0);
-              Iterator<Row> rowIterator = sheet.iterator();
-              athletes = new ArrayList<String>();
+            File deHe = new File("Excel path here");
+            FileInputStream ex = new FileInputStream(deHe);
+            XSSFWorkbook workbook = new XSSFWorkbook(ex);
+            XSSFSheet sheet = workbook.getSheetAt(0);
+            Iterator<Row> rowIterator = sheet.iterator();
+            
+            athletes = new ArrayList<String>();
+            athletes1 = new ArrayList<Athlete>();
 
-              while (rowIterator.hasNext()) {
-                  Row row = rowIterator.next();
-                  Iterator<Cell> cellIterator = row.cellIterator();
-                  while (cellIterator.hasNext()) {
-                      Cell cell = cellIterator.next();
-
-                      if (row.getRowNum() > 1) { 
-                          if (cell.getColumnIndex() == columnIndex) {
-                              switch (cell.getCellType()) {
-                             //     case NUMERIC:
-                             //      athletes.add(cell.getNumericCellValue() + "");
-                             //         break;
-                                  case STRING:
-                                      athletes.add(cell.getStringCellValue());
-                                      break;
-							case BLANK:
-								break;
-							case BOOLEAN:
-								break;
-							case ERROR:
-								break;
-							case FORMULA:
-								break;
-							case _NONE:
-								break;
-							default:
-								break;
-                              }
-                          }
-                      }
-                  }
-              }
-              ex.close();
+            while (rowIterator.hasNext()) {
+                Row row = rowIterator.next();
+                Iterator<Cell> cellIterator = row.cellIterator();
+                while (cellIterator.hasNext()) {
+                    Cell cell = cellIterator.next();
+                    Woman dec = new Woman(firstName, lastName, ID);
+                    switch (cell.getCellType()) {
+                    // Maybe put in separate method?
+                        case NUMERIC:
+                            athletes.add(cell.getNumericCellValue() + "");
+                            break;
+                        case STRING:
+                            athletes.add(cell.getStringCellValue());
+                            
+                            break;
+                        case BLANK:
+                            break;
+                        case BOOLEAN:
+                            break;
+                        case ERROR:
+                            break;
+                        case FORMULA:
+                            break;
+                        case _NONE:
+                            break;
+                        default:
+                            break;
+                    }
+                } //
+            }
+            ex.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
