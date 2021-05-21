99a31c99e9b8fc6eadbd7302d57c7dd705cdcc67
stefanstoimenov <stefan.stoimenov@yahoo.com>
2021-05-21 17:23:52 +0200

    calculation step 1

--- ProjectSportyStuff/src/test/java/stepDefinitions/CalculationSteps.java
+++ ProjectSportyStuff/src/test/java/stepDefinitions/CalculationSteps.java
@@ -3,20 +3,25 @@ package stepDefinitions;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import ourCode.Calculation;
 
 public class CalculationSteps {
 	
-	@Given("I have chosen {string}")
-	public void i_have_chosen_event() {
-		// TODO
+	@Given("I have chosen {string} as an event")
+	public void i_have_chosen_as_an_event(String event) {
+		Calculation chooseEvent = new Calculation();
+		//chooseEvent.CalculateDecathlonResult(0, event);
+		//chooseEvent.CalculateHeptathlonResult(0, event);
 	}
-	@When("I enter the {double}")
-	public void i_enter_the_result() {
-		// TODO
+	@When("I enter the (.*) as a result")
+	public void i_enter_the_as_a_result(double result) {
+		Calculation enterResult = new Calculation();
+		//enterResult.CalculateDecathlonResult(result, null);
+		//enterResult.CalculateHeptathlonResult(result, null);
 	}
 
-	@Then("I get the {int} for {double} and {string}")
-	public void i_get_the_score_for_result_and_event(String message) {
+	@Then("I get the (.*) for (.*) and {string}")
+	public void i_get_the_score_for_result_and_event(String score) {
 		// TODO
 	}
 }
