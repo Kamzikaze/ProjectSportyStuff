371f4bb2fae67aad8d8b3e6f01230c4c06177e04
stefanstoimenov <stefan.stoimenov@yahoo.com>
2021-05-21 14:14:00 +0200

    calculation commit 1

---
+++ ProjectSportyStuff/src/test/java/stepDefinitions/CalculationSteps.java
@@ -0,0 +1,22 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class CalculationSteps {
+	
+	@Given("I have chosen {string}")
+	public void i_have_chosen_event() {
+		// TODO
+	}
+	@When("I enter the {double}")
+	public void i_enter_the_result() {
+		// TODO
+	}
+
+	@Then("I get the {int} for {double} and {string}")
+	public void i_get_the_score_for_result_and_event(String message) {
+		// TODO
+	}
+}
